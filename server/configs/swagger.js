d4a125d5f01d3e6246b85f108ea754b32a7e2b59
chsiao <chsiao3@my.centennialcollege.ca>
2025-11-16 19:32:56 -0500

    fix: swagger sacn route

--- server/configs/swagger.js
+++ server/configs/swagger.js
@@ -1,8 +1,14 @@
 import swaggerJSDoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
 import path from "path";
+import { fileURLToPath } from "url";
 
-const routesPath = path.join(process.cwd(), "routes/*.js");
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const routesPath = path.join(__dirname, "..", "routes", "*.js");
+
+console.log("Swagger scanning :", routesPath);
 const options = {
     definition: {
         openapi: "3.0.0",
