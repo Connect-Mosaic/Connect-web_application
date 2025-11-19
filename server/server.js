15799600f73ac71242b468487d586894a3710e85
chsiao <chsiao3@my.centennialcollege.ca>
2025-11-19 00:00:07 -0500

    refactor : clean code

--- server/server.js
+++ server/server.js
@@ -1,8 +1,6 @@
 import config from "./configs/config.js";
 import app from "./express.js";
 import mongoose from "mongoose";
-import { swaggerSpec, swaggerUiMiddleware } from "./configs/swagger.js";
-import userRoutes from "./routes/user.routes.js";
 
 mongoose.Promise = global.Promise;
 mongoose
@@ -12,12 +10,6 @@ mongoose.connection.on("error", () => {
   throw new Error(`unable to connect to database: ${config.mongoUri}`);
 });
 
-// Mount your user routes HERE
-app.use("/api/user", userRoutes);
-
-app.get("/api/health", (req, res) => {
-  res.json({ message: "Welcome to User application. Server healthy." });
-});
 
 app.listen(config.port, (err) => {
   if (err) {
@@ -26,6 +18,3 @@ app.listen(config.port, (err) => {
   }
   console.info("Server started on port %s.", config.port);
 });
-
-// Swagger
-app.use("/api-docs", swaggerUiMiddleware.serve, swaggerUiMiddleware.setup(swaggerSpec));
