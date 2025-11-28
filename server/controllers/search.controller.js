ce8738cd51848c135b5add93fc2e6b42b7c33761
chsiao <chsiao3@my.centennialcollege.ca>
2025-11-28 16:57:13 -0500

    feat : backend search AP connect to real dataI  #51

--- server/controllers/search.controller.js
+++ server/controllers/search.controller.js
@@ -1,17 +1,28 @@
 import extend from "lodash/extend.js";
 import errorHandler from "./error.controller.js";
 import { successResponse, errorResponse } from '../helpers/apiResponse.js';
-
-// user and event models
+import Event from "../models/events.model.js";
 import User from "../models/user.model.js";
-// import Event from "../models/events.model.js";
 const search = async (req, res) => {
     try {
         console.log('[Search] search called with query:', req.query.q, 'by user id:', req.auth.userId);
         const query = req.query.q;
-        // event not prepare for search yet create dummy array
-        let userResults = ['user1', 'user2'];
-        let eventResults = ['event1', 'event2'];
+        if (!query) {
+            return res.status(400).json(errorResponse('Search query parameter "q" is required'));
+        }
+        // Search users by name 
+        const userResults = await User.find({
+            $or: [
+                { first_name: { $regex: query, $options: 'i' } },
+                { last_name: { $regex: query, $options: 'i' } }
+            ]
+        }).select('first_name last_name email university program interests location profile_picture').exec();
+
+        // Search events by title
+        const eventResults = await Event.find({
+            title: { $regex: query, $options: 'i' }
+        }).select('title location date startTime endTime interests location image').exec();
+
         return res.json(successResponse('Search results retrieved successfully', {
             users: userResults,
             events: eventResults
